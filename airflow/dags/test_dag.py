e1eeea0797da6f41e1b24a5b03f52f7538176448
jayamanikharyono <jayaharyonomanik@gmail.com>
2021-01-24 21:42:49 +0700

    add test dag validation

---
+++ airflow/dags/test_dag.py
@@ -0,0 +1,59 @@
+from airflow import DAG
+from airflow.models import Variable
+from airflow.utils.dates import days_ago
+from airflow.operators.python_operator import PythonOperator
+
+from datetime import timedelta
+
+DAG_ID = "test_dag"
+
+default_args = {
+    'owner' : 'DE',
+    'depends_on_past' : False,
+    'start_date' : days_ago(0),
+    'email' : ['example@123.com'],
+    'email_on_failure' : False,
+    'email_on_retry' : False,
+    'retries' : 0,
+    'retry_delay' : timedelta(minutes=5)
+}
+
+
+dag = DAG(
+    DAG_ID,
+    description = 'sample dag to test dag',
+    default_args = default_args,
+    access_control = {
+        'DE' : {'can_dag_read', 'can_dag_edit'},
+        'BI' : {'can_dag_read'}
+    },
+    schedule_interval = timedelta(days = 1)
+)
+
+
+def test_import_module():
+    import prettyprint
+    return True
+
+
+def test_access_var():
+    my_var = Variable.get("MESSAGE")
+    print("my var message : {}".format(my_var))
+    return ("Access Var Success!")
+
+
+access_var = PythonOperator(
+                    task_id = 'test_access_var',
+                    python_callable = test_access_var,
+                    dag = dag
+                )
+
+
+import_module = PythonOperator(
+                    task_id = 'test_import_module',
+                    python_callable = test_import_module,
+                    dag = dag
+                )
+
+
+access_var >> import_module
