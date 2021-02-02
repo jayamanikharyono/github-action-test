0d06360e8852257250b17b011cddb1e822400a64
Jaya Haryono Manik <jayaharyonomanik@gmail.com>
2021-02-02 22:46:36 +0700

    aaa

---
+++ .github/filter_dags.py
@@ -0,0 +1,25 @@
+import os
+import logging
+import argparse
+
+
+def main(args):
+    logging.info("added and modified : {}".format(args.addmodified))
+    addmodified = args.addmodified
+    excluded_files = set(addmodified[1:-1].split(","))
+    for file in excluded_files:
+        print(file)
+    """
+    for subdir, dirs, files in os.walk('airflow/dags/'):
+        for file in files:
+            if subdir + file not in excluded_files:
+                print(subdir + file)
+                os.remove(subdir + file)
+    """
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(allow_abbrev=False)
+    parser.add_argument('--addmodified', action = 'store', type = str, required = True)
+    args = parser.parse_args()
+    main(args)
